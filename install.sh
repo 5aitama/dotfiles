0d18f8d16b6e927c35fa0e7624a35abbd4b19f6c
Alex <44070842+5aitama@users.noreply.github.com>
2026-04-09 10:08:31 +0200

    Refactor install script to separate installations
    Separated the installation of binstall and Jujutsu, and added setting VSCode as the default editor.

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-# Install binstall & Jujutsu VSC
+# Install binstall
 curl -L --proto '=https' --tlsv1.2 -sSf \
-    https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash && \
+    https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+
+# Install Jujutsu
 cargo binstall --strategies crate-meta-data jj-cli
+
+# Set vscode as default editor
+echo "export EDITOR='code --wait'" >> ~/.bashrc && source ~/.bashrc
